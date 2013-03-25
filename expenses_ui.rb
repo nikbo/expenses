91e873aabb156095bc0b97ecd7e17f9cdd9a0bf6
nikbo <nikita-bobrik@yandex.ru>
2013-03-25 16:36:52 +0300

    Add percents

--- expenses_ui.rb
+++ expenses_ui.rb
@@ -19,7 +19,8 @@ def menu
   until choice == 'e'
     puts "Press 'add' to add expense, press 'list' to list all expenses for all time
           press 'month' to list expenses for last month,
-          press 'tag' to list all expenses of tag"
+          press 'tag' to list all expenses of tag,
+          press 'percent' to list expenses by tag name in percents "
     choice = gets.chomp
     case choice
       when 'list'
@@ -32,6 +33,10 @@ def menu
         list_tag
       when 'exit'
         exit
+      when 'e'
+        true
+      when 'percent'
+        list_percent
       else
         invalid
     end
@@ -68,11 +73,25 @@ end
 def list_month
   m = prompt 'How many months back?'
   m=m.to_i
-  month_choice = Expense.where("created_at < ?", m.month.ago)
+  month_choice = Expense.where("created_at < ?", m.months.ago)
   month_choice.all.each do |expense|
   puts "#{expense.id.to_s}. #{expense.name.to_s} #{expense.cost.to_s}         #{expense.created_at.to_s}"
 
   end
+end
+
+def list_percent
+  tag = prompt 'Enter the name of tag'
+  sum_1=0
+  sum_2=0
+  tag_choice = Expense.where(:tag => tag)
+  tag_choice.all.each do |expense|
+    sum_1+=expense.cost
+  end
+  Expense.all.each do |expense|
+    sum_2+=expense.cost
+  end
+  puts "#{(sum_1*100)/sum_2}% of your money you've spent on #{tag}"
 end
 
 def invalid
